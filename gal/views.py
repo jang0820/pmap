250149cc86695cec8defa28c4d9628159f4a41ff
jang0820 <jang0820@gmail.com>
2024-07-10 00:36:15 +0800

    新增相簿分頁功能

--- gal/views.py
+++ gal/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from map.models import Img
 from django.views.generic import TemplateView, ListView
 from pmap import settings
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
 import os
 
 class GalView(TemplateView): #將指定資料夾下不是tH開頭的圖片製作成相簿
@@ -25,4 +28,14 @@ class GalListView(ListView): #列出media/img下的所有子資料夾
         imgpath = str(settings.BASE_DIR)+ "/media/img/"
         dirs = [d for d in os.listdir(imgpath) if os.path.isdir(os.path.join(str(settings.BASE_DIR)+ "/media/img/", d))] #找出子資料夾
         context["dirs"] = dirs
+        limit = 10  #每個分頁10個資料夾
+        paginator = Paginator(dirs, limit)  #設定分頁的資料夾數量
+        page = self.request.GET.get('page')  #取出網址的page
+        try:
+            dirp = paginator.page(page)  #指定第幾個page
+        except PageNotAnInteger:  #page不是整數
+            dirp = paginator.page(1)  #指定第一個分頁
+        except EmptyPage:  #不存在的分頁
+            dirp = paginator.page(1)  #指定第一個分頁
+        context["dirp"] = dirp
         return context
